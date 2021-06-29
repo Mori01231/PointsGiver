2f9c3f3cd9b7dbe2ea801eaca89ea317df24ff7c
Mori01231 <mori01231mc@gmail.com>
2021-06-30 00:24:48 +0900

    [Add] Automatically give points

--- src/main/java/com/github/mori01231/pointsgiver/PointsGiver.java
+++ src/main/java/com/github/mori01231/pointsgiver/PointsGiver.java
@@ -1,18 +1,42 @@
 package com.github.mori01231.pointsgiver;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PointsGiver extends JavaPlugin {
 
+    private static PointsGiver instance;
+
+    public PointsGiver (){
+        instance = this;
+    }
+
+    public static PointsGiver getInstance() {
+        return instance;
+    }
+
+
     @Override
     public void onEnable() {
         getLogger().info("PointsGiver has been enabled.");
-        this.getCommand("givepoints").setExecutor(new PointsGiverCommand());
+        this.getCommand("pointsgive").setExecutor(new PointsGiverCommand());
 
+        int PointsAmount = getConfig().getInt("points_amount");
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            @Override
+            public void run() {
+                sendCommand("pointsgive " + PointsAmount);
+            }
+        }, 0L, 24000L); //0 Tick initial delay, 24000 Tick (20 minutes) between repeats
     }
 
     @Override
     public void onDisable() {
         getLogger().info("PointsGiver has been disabled.");
+    }
+
+    public void sendCommand(String command){
+        getServer().dispatchCommand(getServer().getConsoleSender(), command);
     }
 }
--- src/main/java/com/github/mori01231/pointsgiver/PointsGiverCommand.java
+++ src/main/java/com/github/mori01231/pointsgiver/PointsGiverCommand.java
@@ -1,31 +1,72 @@
 package com.github.mori01231.pointsgiver;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import static org.bukkit.Bukkit.getLogger;
 import static org.bukkit.Bukkit.getServer;
 
 public class PointsGiverCommand implements CommandExecutor {
+
+    private final PointsGiver plugin = PointsGiver.getInstance();
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        int givePoints = 0;
+        // check for permissions
         try{
-            givePoints = Integer.parseInt(args[0]);
+            Player p = (Player) sender;
+            if(p instanceof Player){
+                getLogger().info("Is player");
+                if(!sender.hasPermission("pointsgive.give")){
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c権限不足"));
+                    return true;
+                }
+            }
         }catch(Exception e){
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cThe argument must be an integer."));
-            return true;
+            // sender is console
         }
 
-        for(Player player : Bukkit.getServer().getOnlinePlayers()){
-            String playerName = player.getDisplayName();
-            getServer().dispatchCommand(getServer().getConsoleSender(), "points give " + playerName + " " + givePoints);
+
+        // determine the amount of points to give
+        int givePointsTemp;
+        getLogger().info("Sending PlayerPoints.");
+        try{
+            givePointsTemp = Integer.parseInt(args[0]);
+        }catch(Exception e){
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cThe first argument must be an integer."));
+            return true;
         }
-        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&3Gave all players in server " + givePoints + " points."));
+        final int givePoints = givePointsTemp;
+
+        // actually give the points
+        getLogger().info("Running bukkit runnable");
+        int i = 1;
+        for(Player player : getServer().getOnlinePlayers()){
+            try{
+                String playerName = player.getName();
+                // 1 second delay in between each player getting their points
+                i+= 20;
+                if(playerName != null){
+                    new BukkitRunnable() {
+                        @Override
+                        public void run() {
+                            getServer().dispatchCommand(getServer().getConsoleSender(), "points give " + playerName + " " + givePoints);
+                            getLogger().info("Gave " + playerName + " " + givePoints + " PlayerPoints.");
+                        }
+                    }.runTaskLater(plugin, i);
+
+                }
+            }catch(Exception e){
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cException"));
+            }
+        }
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&3Gave all players in server " + givePoints + " PlayerPoints."));
+
         return true;
     }
 }
